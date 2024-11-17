54bde3d024ffec7b3297c73b2822a248a483f4c4
Allison Emily Coleman <allison@parseceducation.com>
2024-11-16 20:41:29 -0800

    feat: classify if transcript cooking related

--- src/recipe_bot/scraper/recipe_generator.py
+++ src/recipe_bot/scraper/recipe_generator.py
@@ -3,13 +3,40 @@ import logging
 import openai
 from config.config import OPENAI_API_KEY
 
+openai.api_key = OPENAI_API_KEY
+
 
 class RecipeGenerator:
     def __init__(self, output_dir="recipes"):
         self.output_dir = output_dir
-        openai.api_key = OPENAI_API_KEY
+
+    def classify_transcript(self, transcript, caption):
+        prompt = (
+            f"Determine if the following transcript and instagram caption is related to cooking and likely to contain a recipe. "
+            f"Respond with a percentage likelihood:\n\n"
+            f"Transcript:\n{transcript}\n\n"
+            f"Caption:\n{caption}\n\n"
+            f"Response format: 'Likelihood: X%'"
+        )
+        try:
+            response = openai.chat.completions.create(
+                model="gpt-4o-mini", messages=[{"role": "user", "content": prompt}]
+            )
+            likelihood_str = response.choices[0].message.content
+            likelihood = int(likelihood_str.split(":")[1].strip().replace("%", ""))
+            return likelihood
+        except Exception as e:
+            logging.error(f"Error during classification: {e}")
+            return 0
 
     def generate_recipe(self, transcript, caption, save=True, shortcode=None):
+        likelihood = self.classify_transcript(transcript, caption)
+        if likelihood < 85:
+            logging.info(
+                "The transcript is not likely to contain a recipe. Please try another video."
+            )
+            return ""
+
         prompt = (
             f"Given the following transcript and caption, extract an actionable recipe:\n\n"
             f"Transcript:\n{transcript}\n\n"
